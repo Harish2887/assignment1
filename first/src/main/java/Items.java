1237b1cd6515d512d0f6ffd6f0daee5916d9798b
Harish Kumar <harish.kumar@kindredgroup.com>
2021-09-24 14:55:49 +0200

    first test

---
+++ first/src/main/java/Items.java
@@ -0,0 +1,17 @@
+enum ItemTypes {
+  raw, manufactured, imported
+}
+
+public class Items {
+    private String name;
+    private Float price;
+    private ItemTypes type;
+    private Integer quantity;
+    private Float tax;
+    private Float totalPrice;
+
+    public Float calculateTax() {
+
+    }
+
+}
